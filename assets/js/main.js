842fde8788f73e07612cb7984b73a2791de1def9
John Carlo <johncarlobalcita.business@gmail.com>
2025-09-29 14:50:41 +0800

    update 9/29/25
    update 9/29/25

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,11 +1,21 @@
 $(document).ready(function () {
-    $("#contactBtn").on("click", function () {
+    slideFocus('#aboutBtn', '#about')
+    slideFocus("#contactBtn", "#contact");
+    slideFocus('#skillBtn', '#skills')
+    slideFocus("#projectBtn", "#projects");
+    slideFocus('#experienceBtn', '#experience')
+})
+
+function slideFocus(id, section) {
+    $(id).on("click", function () {
         $("html, body").animate({
-            scrollTop: $("#contact").offset().top
+            scrollTop: $(section).offset().top - 200
         }, 600, function () {
-            $("#contact").focus(); // focus on section
-            // Or focus directly on first input:
-            // $("#contactSection input:first").focus();
+            $(section).focus();
+
+            // highlight the active link
+            $(".nav-link").removeClass("active");
+            $(id).addClass("active");
         });
     });
-})
+}
